cfcacf611707d9f03fb85771828932f5da60d982
semicognizant <zacka@tildenprep.com>
2019-01-10 11:52:20 -0800

    Adding ChoiceQuestion

---
+++ TG/src/testing_ground_pkg/ChoiceQuestion.java
@@ -0,0 +1,28 @@
+package testing_ground_pkg;
+
+import java.util.ArrayList;
+
+public class ChoiceQuestion extends Question {
+
+	private ArrayList<String> choices;
+	
+	public ChoiceQuestion() {
+		choices = new ArrayList<String>();
+		
+	}
+	
+	public void addChoice(String choice, boolean correct) {
+		choices.add(choice);
+		if (correct) {
+			String choiceString = "" + choices.size();
+			setAnswer(choiceString);
+		}
+	}
+	
+	public void display() {
+		super.display();
+		for(int i=0; i < choices.size(); i++) {
+			int choiceNumber = i+1;
+		}
+	}
+}
